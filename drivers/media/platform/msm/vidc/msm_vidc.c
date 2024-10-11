7bd0cd82ebafc8bcc44c1131bb35ad78d0c696b3
ellenoireQ <166298239+ellenoireQ@users.noreply.github.com>
2024-10-11 03:52:49 +0000

    Treeqide: fix error build

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1532,17 +1532,16 @@ int msm_vidc_destroy(struct msm_vidc_inst *inst)
 	kfree(inst);
 	return 0;
 }
-
-int msm_vidc_close(void *instance)
+void close_helper(struct kref *kref)
 {
-	void close_helper(struct kref *kref)
-	{
 		struct msm_vidc_inst *inst = container_of(kref,
 				struct msm_vidc_inst, kref);
 
 		msm_vidc_destroy(inst);
-	}
+}
 
+int msm_vidc_close(void *instance)
+{
 	struct msm_vidc_inst *inst = instance;
 	struct buffer_info *bi, *dummy;
 	int rc = 0, i = 0;
